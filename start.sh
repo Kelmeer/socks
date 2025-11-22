36cb5744070e171281156545ec2c7c5e1d6b8e5e
Kelmeer <48247877+Kelmeer@users.noreply.github.com>
2025-11-22 17:42:54 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# Ждём секунду, чтоб health.py успел подняться (на всякий случай)
+sleep 2
+
 mitmdump --mode socks5 \
   --listen-host 0.0.0.0 \
   --listen-port 1080 \
-  --set socks_proxy_auth=require:user:4958
+  --set socks_proxy_auth=require:qwe:321 \
+  --quiet
